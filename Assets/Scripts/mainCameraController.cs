2a60b5809330440f4675a778bb8fe048e0a49595
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2018-11-30 15:48:43 -0200

    initial Commit

---
+++ Assets/Scripts/mainCameraController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class mainCameraController : MonoBehaviour {
+	//tempo acumulado para controlar o tempo de criar uma nova comida
+	private float timeSpaw=0f;
+    //tempo para criar uma nova comida
+    public float spawFoodInterval = 3;
+
+	//prefab da comida
+    public GameObject foodPrefab;
+	//prefab de uma parede no mapa
+    public GameObject cellPrefab;
+    public GameObject headPrefab;
+
+	//limites do cenario
+    public Transform borderTop;
+	public Transform borderBottom;
+	public Transform borderLeft;
+	public Transform borderRight;
+
+	//UI Text para exibir os pontos
+    public Text pointsLabel;
+	//A cabeca da cobra
+    private GameObject head;
+	//A matriz que compoe o cenario com celulas vazias e ocupadas por paredes
+    private int[,] mat;
+
+    // Use this for initialization
+    void Start () {
+		//pega o nome do para carregar
+        TextAsset mapConfig = Resources.Load("mapConfig") as TextAsset;
+        
+
+        string text = mapConfig.text;
+        string nameMap = text.Split('.')[0];
+		
+		//Abre o arquivo do mapa (necessariamente o mapa tem que ter 40 celulas na horizontal por 20 celulas na vertical)
+		//O é uma celula vazia e 1 é uma celula onde tem parede
+        TextAsset map = Resources.Load(nameMap) as TextAsset;
+         mat = new int[40,20];
+        
+		//carrega o mapa na matriz
+        string textMapa = map.text;
+        string[] lines = textMapa.Split('\n');
+        for (int i = 0; i < 20; i++)
+        {
+           
+            string[] col = lines[i].Split(',');
+            for (int j = 0; j < 40; j++)
+            {
+                string value = col[j];
+                if (value == "1")
+                {
+                    Instantiate(cellPrefab).transform.Translate(new Vector2(j, 19-i));
+                }
+                mat[j,19-i]= int.Parse(value);
+            }
+        }
+		//instancia a cabeca da cobra no mapa
+        head = Instantiate(headPrefab);
+        head.GetComponent<snakeController>().PointsLabel = pointsLabel;
+        head.transform.Translate(new Vector2(20, 10));
+    }
+	//Funcao que verifica se a posicao aonde vai spawnar a comida não é uma parede
+    public bool isOccuped(int x, int y)
+    {
+        if ((x > 39 || x < 0) || (y > 19 || y < 0))
+        {
+            return true;
+        }
+        else if (mat[x, y] == 0)
+        {
+            return false;
+        }
+        return false;
+    }
+    private void Update()
+    { 	//verifica se não ocorreu game over com a cobra
+        if (!head.GetComponent<snakeController>().GameOver)
+        {   //Acumula o tempo de spawn para a nova comida
+            timeSpaw += Time.deltaTime;
+            if (timeSpaw / (spawFoodInterval) > 1)//se o tempo de spawn acumulado for maior que o tempo de spawn então cria uma nova comida
+            {
+                timeSpaw = 0;
+                bool ocuped = true;
+                int x = 0, y = 0;
+                while (ocuped)//enquanto a posição da comida for uma parede sorteia outra posicao
+                {
+                    x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
+                    y = (int)Random.Range(borderTop.position.y, borderBottom.position.y);
+                    ocuped = isOccuped(x, y);
+                }
+				//instancia a comida e coloca na posição sorteada
+                Instantiate(foodPrefab).transform.Translate(new Vector2(x, y));
+            }
+        }
+       
+    }
+}
---
+++ Assets/Scripts/snakeController.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class snakeController : MonoBehaviour {
+	//prefab dos elementso para se adicionar na calda
+	public GameObject tailPrefab;
+	//lista de objetos que é a calda
+    private List<Transform> tail;
+	//variavel para controlar se uma comida foi pega pela cabeca
+    bool ate;
+	//direcao incial da cobra
+	private Vector2 dir = Vector2.right;
+	//Ui text para exibir os bpontos
+    private Text text;
+    private bool gameOver;
+
+    public Text PointsLabel
+    {
+        get { return text; }
+        set { text = value; }
+    }
+    public bool GameOver
+    {
+        get { return gameOver; }
+        set { gameOver = value; }
+    }
+
+    // Use this for initialization
+    void Start () {
+        gameOver = false;
+        tail = new List<Transform>();
+        ate = false;
+		//Registra o calbeck para a funcao de movimentacao da cobra 
+        InvokeRepeating("move", 1, 0.2f);
+        Instantiate(text);
+        text.text = "Points: " + tail.Count;
+
+    }    
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.name.StartsWith ("FoodPrefab")) {//se colidiu com uma comida destroi a comida e seta a variavel ate para true
+            ate = true;
+			Destroy (coll.gameObject);
+		} else {//se colidiu com algo que não e uma comida game over
+            gameOver = true;
+        }
+	}
+	
+	//Funcao serve somente para pegar as novas direcoes da cobra
+	void Update() {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (verifyDirection(dir, Vector2.right)){
+                dir = Vector2.right;
+            }
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (verifyDirection(dir,- Vector2.up))
+            {
+
+                dir = -Vector2.up;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (verifyDirection(dir, -Vector2.right))
+            {
+
+                dir = -Vector2.right;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (verifyDirection(dir, Vector2.up))
+            {
+
+                dir = Vector2.up;
+            }
+        }
+        
+       
+    }
+	//verifica se a cobra pode voltar (caso tenha ao menos um elemento na cauda ela não pode mais inverter a direcao atual)
+    private bool verifyDirection(Vector2 direction, Vector2 newDirection)
+    {
+        if ((tail.Count > 0)&& newDirection == -direction)
+        {
+            return false;
+        }
+        return true;
+    }
+   
+
+    public void move()
+    {
+        if (!gameOver)
+        { 	
+            Vector2 v = transform.position;
+            transform.Translate(dir);
+            if (ate)//instancia um novo elemento na cauda
+            {
+                GameObject g;
+                g = Instantiate(tailPrefab, v, Quaternion.identity);
+                tail.Insert(0, g.transform);
+                text.text = "Points: " + tail.Count;
+                ate = false;
+            }
+          
+			//acerta a posicao dos elementos da cauda
+            if (tail.Count > 0)
+            {
+                Vector2 v2 = tail[0].position;
+                tail[0].position = v;
+                for (int i = 1; i < tail.Count; i++)
+                {
+                    Vector2 aux = tail[i].position;
+                    tail[i].position = v2;
+                    v2 = aux;
+                }
+            }
+        }
+    }
+}
